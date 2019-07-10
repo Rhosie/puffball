fbb736b7d0c895acea500ea2d334fd2cd214b9f4
rhosie <rhosie383@outlook.com>
2019-07-10 22:21:01 +0100

    Updates

---
+++ precache-manifest.ce777a8c5dd14a8923f9dc3c84f1cefa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57778cabbf21ab33a0e28aeddea01b9c",
+    "url": "/puffball/index.html"
+  },
+  {
+    "revision": "259aa36b40c78e36b2e4",
+    "url": "/puffball/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/puffball/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "259aa36b40c78e36b2e4",
+    "url": "/puffball/static/js/main.e1360b2c.chunk.js"
+  },
+  {
+    "revision": "0b4297e204c7f18510f1",
+    "url": "/puffball/static/js/runtime~main.740690bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/puffball/static/media/logo.5d5d9eef.svg"
+  }
+]);
