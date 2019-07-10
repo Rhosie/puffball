17e47274bf12ce99ae1d38871d581d504d7c6abe
rhosie <rhosie383@outlook.com>
2019-07-10 22:09:54 +0100

    I have added an image and changed the text. I have set up the project.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Felix is a trap and traps are gay.
+        </p>
+      
+        <img src="/images/necky.jpeg"/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
