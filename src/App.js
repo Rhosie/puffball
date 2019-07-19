e8ed33aba184980ac0cff67119dcc96c0121da4b
rhosie <rhosie383@outlook.com>
2019-07-19 18:33:23 +0100

    added pretty styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {Button} from '@material-ui/core'
-import './App.css';
+import React from 'react'
+import { Button } from '@material-ui/core'
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <div className="imgbox">
-       
-        <p style={{fontSize:"50px"}} className = "pigpink">
-          Pigs are the best!
-        </p>
-        <p >
-        Welcome to this page!
-        </p>
-      <Button variant="outlined" color="secondary" >
-          About this page
-      </Button>
-        
-        
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="imgbox">
+                <p style={{ fontSize: '50px' }} className="pigpink">
+                    Pigs are the best!
+                </p>
+                <p>Welcome to this page!</p>
+                <Button variant="outlined" color="secondary">
+                    About this page
+                </Button>
+                <Button variant="outlined" color="secondary">
+                    Trivia Quiz
+                </Button>
+                <Button variant="outlined" color="secondary">
+                    Pigtures
+                </Button>
+            </div>
+        </div>
+    )
 }
 
-export default App;
-
+export default App
